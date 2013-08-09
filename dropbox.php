6f164490d2bdb079bf02f3c3540335824af48eaf
Ismail Fazal <ismailfazal@gmail.com>
2013-08-09 11:59:08 +0530

    Delete dropbox.php

--- dropbox.php
+++
@@ -1,4 +0,0 @@
-include("DropboxUploader.php");
-/*** ... ***/
-$uploader = new DropboxUploader("qh@india.com", "openpass");
-$uploader->upload("upload/".$file_on_your_server, "Public/dropBox_directory", null); 
